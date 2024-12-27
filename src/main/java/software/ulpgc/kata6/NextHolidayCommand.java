f742f22debb53e549ed07319ac12ff2acf46d12c
maria <informatica1bachIES@gmail.com>
2024-12-27 10:11:35 +0000

    First commit, command classes created

---
+++ src/main/java/software/ulpgc/kata6/NextHolidayCommand.java
@@ -0,0 +1,38 @@
+package software.ulpgc.kata6;
+
+import java.time.LocalDate;
+
+public class NextHolidayCommand implements  Command{
+    private final Input input;
+    private final Output output;
+
+    public NextHolidayCommand(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Override
+    public void execute() {
+        LocalDate current = input.start();
+        LocalDate nextHoliday = current.plusDays(1);
+        while(new WorkingDaysCalendar().iteratorFor(current).hasNext()){
+            nextHoliday = nextHoliday.plusDays(1);
+            if (isHoliday(nextHoliday)){
+                break;
+            }
+        }
+        output.nextHoliday(nextHoliday);
+    }
+
+    private boolean isHoliday(LocalDate date){
+        return date.getDayOfWeek().getValue() == 7 || date.getDayOfWeek().getValue() == 6;
+    }
+
+    public interface Input{
+        LocalDate start();
+    }
+
+    public interface Output{
+        void nextHoliday(LocalDate date);
+    }
+}
